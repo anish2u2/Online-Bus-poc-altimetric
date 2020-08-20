a6431b341640324cd38348e7aaaa4639058a1a7d
Anish Singh <anish2u2@gmail.com>
2020-08-20 22:57:49 +0530

    Added test cases

--- OnlineBusBooking/src/test/java/org/online/booking/bus/OnlineBusBookingApplicationTests.java
+++ OnlineBusBooking/src/test/java/org/online/booking/bus/OnlineBusBookingApplicationTests.java
@@ -1,13 +1,16 @@
 package org.online.booking.bus;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+//@SpringBootTest
+@RunWith(SpringRunner.class)
 class OnlineBusBookingApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("Executing test cases..");
 	}
 
 }
